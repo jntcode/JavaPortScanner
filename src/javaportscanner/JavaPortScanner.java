4fc7dcee1c31ff563a9bf4194cb80307831fc99a
jntcode <volken1337@gmail.com>
2026-08-15 17:04:46 -0300

    Refactor port scanning with methods and enum status

--- src/javaportscanner/JavaPortScanner.java
+++ src/javaportscanner/JavaPortScanner.java
@@ -8,6 +8,8 @@ import java.net.SocketTimeoutException;
 import java.net.ConnectException;
 import java.net.UnknownHostException;
 import java.net.InetAddress;
+import java.io.IOException;
+
 
 public class JavaPortScanner {
 
@@ -17,40 +19,81 @@ public class JavaPortScanner {
         String ipAddress = scanner.nextLine();
         try {
             InetAddress.getByName(ipAddress);
-        } catch (UnknownHostException) {
+        } catch (UnknownHostException e) {
             System.out.println("Invalid IP address.");
             return;
         }
+
         System.out.println("Scanning IP: " + ipAddress);
         System.out.println("Enter starting port: ");
-        int startingPort = Integer.valueOf(scanner.nextLine());
-        System.out.println("Enter ending port: ");
-        int endingPort = Integer.valueOf(scanner.nextLine());
-        if (endingPort < startingPort || startingPort < 1 || endingPort > 65535) {
-            System.out.println("Error: Enter a valid port range (1-65535)");
+
+        int startingPort;
+        int endingPort;
+
+        try {
+            startingPort = Integer.valueOf(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Enter a valid port number.");
             return;
 
         }
-        for (int i = startingPort; i <= endingPort; i++) {
-            Socket socket = new Socket();
+        System.out.println("Enter ending port: ");
 
-            try (SocketAddress address = new InetSocketAddress(ipAddress, i)) {
-                
-
-                socket.connect(address, 1000);
-                System.out.println("Port " + i + ": OPEN");
-
-            } catch (ConnectException) {
-                System.out.println("Port " + i + ": CLOSED");
-            } catch (SocketTimeoutException) {
-                System.out.println("Port " + i + ": FILTERED");
-
-            
-
-            }
-
+        try {
+            endingPort = Integer.valueOf(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Enter a valid port number.");
+            return;
+        }
+        if (endingPort < startingPort
+                || startingPort < 1
+                || startingPort > 65535
+                || endingPort < 1
+                || endingPort > 65535) {
+            System.out.println("Error: Enter a valid port range (1-65535)");
+            return;
         }
 
+        scanPorts(ipAddress, startingPort, endingPort);
+
+    }
+
+    public static void scanPorts(String ipAddress, int startingPort, int endingPort) {
+        for (int port = startingPort; port <= endingPort; port++) {
+            PortStatus result = isPortOpen(ipAddress, port);
+            System.out.println(port + ": " + result);
+
+        }
+    }
+
+    public static PortStatus isPortOpen(String ipAddress, int port) {
+        try (Socket socket = new Socket()) {
+
+            SocketAddress address = new InetSocketAddress(ipAddress, port);
+            socket.connect(address, 1000);
+
+            return PortStatus.OPEN;
+
+        } catch (ConnectException e) {
+
+            return PortStatus.CLOSED;
+
+        } catch (SocketTimeoutException e) {
+
+            return PortStatus.FILTERED;
+
+        } catch (IOException e) {
+
+            return PortStatus.ERROR;
+        }
+
+    }
+
+    enum PortStatus {
+        OPEN,
+        CLOSED,
+        FILTERED,
+        ERROR
     }
 
 }
